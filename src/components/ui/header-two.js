c5da97abeaab5de7862e87c6a843d298eca63b2e
sultanuddin002 <shaikh.sultan.uddin@gmail.com>
2022-08-30 16:49:14 +0500

    fixed responsive for header

--- src/components/ui/header-two.js
+++ src/components/ui/header-two.js
@@ -1,32 +1,32 @@
-import * as React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
 import Tabs from "@mui/material/Tabs";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 import Tab from "@mui/material/Tab";
 import Grid from "@mui/material/Grid";
 import theme from "./theme";
 import { makeStyles } from "@mui/styles";
-import logo from "../../images/logo-main.png";
-import { ClassNames } from "@emotion/react";
+import logo from "../../images/logo-resize.png";
 import { Link } from "gatsby";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 // icons
 import PhoneIcon from "@mui/icons-material/Phone";
-import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import EmailIcon from "@mui/icons-material/Email";
 import WatchLaterIcon from "@mui/icons-material/WatchLater";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const routes = [
   {
@@ -54,90 +54,177 @@ const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const useStyles = makeStyles((theme) => ({
   logo: {
-    width: "10%",
+    // width: "10%",
+  },
+  socialContainer: {
+    [theme.breakpoints.up("md")]: {
+      justifyContent: "flex-end",
+    },
+    [theme.breakpoints.down("md")]: {
+      justifyContent: "center",
+    },
+  },
+  topbarSection: {
+    [theme.breakpoints.down("md")]: {
+      height: "150px",
+    },
+    [theme.breakpoints.up("md")]: {
+      height: "50px",
+    },
+    [theme.breakpoints.down("xs")]: {},
+  },
+  contactContainer: {
+    [theme.breakpoints.up("md")]: {
+      justifyContent: "flex-start",
+      alignItems: "center",
+    },
+    [theme.breakpoints.down("md")]: {
+      justifyContent: "flex-start",
+      direction: "column",
+      spacing: 1,
+    },
   },
 }));
+
+const desktopMenu = (
+  <Tabs
+    sx={{
+      //   marginRight: "auto",
+      marginLeft: "auto",
+    }}
+  >
+    {routes.map((route) => (
+      <Tab
+        component={Link}
+        to={route.link}
+        label={route.name}
+        key={route.name}
+        sx={{
+          color: theme.palette.secondary.main,
+          ...theme.typography.body1,
+          textTransform: "capitalize",
+        }}
+      />
+    ))}
+  </Tabs>
+);
+
+function IconSection(props) {
+  return (
+    <Grid container spacing={2} justifyContent="flex-start" alignItems="center">
+      <Grid item>{props.children}</Grid>
+      <Grid item>
+        <Typography variant="body2" sx={{ color: "#fff" }}>
+          {props.text}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+}
 
 const HeaderTwo = () => {
   const classes = useStyles();
 
+  // Drawer element here
+  const [state, setState] = useState({
+    left: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+      sx={{ width: 250 }}
+    >
+      <List>
+        {routes.map((text, index) => (
+          <ListItem key={text.name} disablePadding>
+            <ListItemButton component={Link} to={text.link}>
+              <ListItemText primary={text.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  // min-width(0px) and (max-width:599px) : for mobiles
+
+  // min-width(600px) and (max-width:899px) : for tablet
+  const mobileSizeMatches = useMediaQuery((theme) =>
+    theme.breakpoints.down("md")
+  );
+
+  // const tabletSizeMatches = useMediaQuery((theme) =>
+  //   theme.breakpoints.between("sm", "md")
+  // );
+
+  const laptopSizeMatches = useMediaQuery((theme) =>
+    theme.breakpoints.up("md")
+  );
   return (
     // Add top bar here
     <>
       <Grid
         container
+        direction="column"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+      >
+        <span>{`theme.breakpoints.up('sm') matches: ${mobileSizeMatches}`}</span>
+        <span>{`theme.breakpoints.up('sm') matches: ${laptopSizeMatches}`}</span>
+      </Grid>
+      <Grid
+        container
         justifyContent="space-around"
         alignItems="center"
-        sx={{ height: "50px", backgroundColor: theme.palette.secondary.main }}
+        classes={{ root: classes.topbarSection }}
+        sx={{ backgroundColor: theme.palette.secondary.main }}
       >
-        <Grid item md={9} xs={6}>
+        <Grid item md={9} xs={8}>
           <Grid
             container
-            spacing={4}
-            justifyContent="flex-start"
-            alignItems="center"
+            columnSpacing={{ md: 4, xs: 1 }}
+            // justifyContent="flex-start"
             sx={{ paddingLeft: "30px" }}
+            classes={{ root: classes.contactContainer }}
           >
             <Grid item>
-              <Grid
-                container
-                spacing={2}
-                justifyContent="flex-start"
-                alignItems="center"
-              >
-                <Grid item>
-                  <PhoneIcon sx={{ color: theme.palette.primary.main }} />
-                </Grid>
-                <Grid item>
-                  <Typography variant="body2" sx={{ color: "#fff" }}>
-                    555-555-555
-                  </Typography>
-                </Grid>
-              </Grid>
+              <IconSection text="555-555-555">
+                <PhoneIcon sx={{ color: theme.palette.primary.main }} />
+              </IconSection>
             </Grid>
             <Grid item>
-              <Grid
-                container
-                spacing={2}
-                justifyContent="flex-start"
-                alignItems="center"
-              >
-                <Grid item>
-                  <EmailIcon sx={{ color: theme.palette.primary.main }} />
-                </Grid>
-                <Grid item>
-                  <Typography variant="body2" sx={{ color: "#fff" }}>
-                    admin@example.com
-                  </Typography>
-                </Grid>
-              </Grid>
+              <IconSection text="mail@example.com">
+                <EmailIcon sx={{ color: theme.palette.primary.main }} />
+              </IconSection>
             </Grid>
             <Grid item>
-              <Grid
-                container
-                spacing={2}
-                justifyContent="flex-start"
-                alignItems="center"
-              >
-                <Grid item>
-                  <WatchLaterIcon sx={{ color: theme.palette.primary.main }} />
-                </Grid>
-                <Grid item>
-                  <Typography variant="body2" sx={{ color: "#fff" }}>
-                    Open 24/7
-                  </Typography>
-                </Grid>
-              </Grid>
+              <IconSection text="24/7 everyday">
+                <WatchLaterIcon sx={{ color: theme.palette.primary.main }} />
+              </IconSection>
             </Grid>
           </Grid>
         </Grid>
 
-        <Grid item md={3} xs={6}>
+        <Grid item md={3} xs={4}>
           <Grid
             container
             spacing={3}
-            justifyContent="flex-end"
             sx={{ paddingRight: "40px" }}
+            classes={{ root: classes.socialContainer }}
           >
             <Grid item>
               <TwitterIcon sx={{ color: theme.palette.primary.main }} />
@@ -157,48 +244,26 @@ const HeaderTwo = () => {
         <Container maxWidth="xl">
           <Toolbar disableGutters>
             <img src={logo} alt="logo" className={classes.logo} />
-            <Tabs
-              sx={{
-                //   marginRight: "auto",
-                marginLeft: "auto",
-              }}
-            >
-              {routes.map((route) => (
-                <Tab
-                  component={Link}
-                  to={route.link}
-                  label={route.name}
-                  key={route.name}
+            {mobileSizeMatches ? (
+              <IconButton sx={{ marginLeft: "auto" }}>
+                <MenuIcon
                   sx={{
-                    color: theme.palette.secondary.main,
-                    ...theme.typography.body1,
-                    textTransform: "capitalize",
+                    color: "#000",
+                    fontSize: "40px",
                   }}
+                  onClick={toggleDrawer("left", true)}
                 />
-              ))}
-            </Tabs>
-
-            <AccessAlarmIcon
-              sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
-            />
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href=""
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              LOGO
-            </Typography>
+                <Drawer
+                  anchor="left"
+                  open={state["left"]}
+                  onClose={toggleDrawer("left", false)}
+                >
+                  {list("left")}
+                </Drawer>
+              </IconButton>
+            ) : (
+              desktopMenu
+            )}
           </Toolbar>
         </Container>
       </AppBar>
